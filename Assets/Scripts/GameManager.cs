45dfd3b82536632330b9fc2c67e22a7395f138b1
Franck-Nein <franco600@live.com.ar>
2021-03-12 13:38:30 -0300

    test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Renderer bg;
+    public Renderer cloud1;
+    public Renderer cloud2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            //Mover BG
+            bg.material.mainTextureOffset = bg.material.mainTextureOffset + new Vector2(0.015f, 0) * Time.deltaTime;
+            cloud1.material.mainTextureOffset = cloud1.material.mainTextureOffset + new Vector2(0.029f, 0) * Time.deltaTime;
+            cloud2.material.mainTextureOffset = cloud2.material.mainTextureOffset + new Vector2(0.049f, 0) * Time.deltaTime;
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpStr;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isJump", true);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpStr));
+	}
+        /*if (!Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isJump", false);
+	}*/
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+	    animator.SetBool("isBorn", true);
+/*	    Debug.Log(gameObject.transform.position.y + " : " + 0.0f);	 */
+        
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+	    animator.SetBool("isRun", true);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(30,0));
+        if (!Input.GetKeyDown(KeyCode.D))
+        {
+	    animator.SetBool("isRun", false);
+        }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+     {
+        if (collision.gameObject.tag == "ground")
+        animator.SetBool("isGround", false);
+    }
+}
